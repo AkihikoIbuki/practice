34e9a741fefb4b5d2efd5bc23f0ed31fdb67f8ab
AkihikoIbuki <97494624+AkihikoIbuki@users.noreply.github.com>
2022-01-12 13:46:36 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,15 @@
+public abstract class TangibleAsset {
+ private String name;
+  private int price;
+  private string color;
+  
+ public TangbleAsset(String name, int price, String color){
+ this.name = name;
+ this.price = price;
+ this.color = color;
+ }
+   
+   public String getName(){return this.name;}
+   public int getPrice(){return this.price;}
+   public int getPrice(){return this.color;}
+}
